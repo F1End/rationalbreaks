9c610517140833cc3ab48847d59892f11ace245f
F1End <horvirich@gmail.com>
2024-09-24 20:54:17 +0200

    -Refactored timers.py. Now working as planned.

--- rationalbreaks/timers.py
+++ rationalbreaks/timers.py
@@ -8,88 +8,54 @@ class RatioNalTimer:
         self._status = "Not started"
         self._cycle_timestamps = []
         self._current_cycle_time = timedelta(0)
-        self._current_worked = timedelta(0)
-        self._carried_worked = timedelta(0)
-        self._current_rest = timedelta(0)
-        self._carried_rest = timedelta(0)
-        self._overrest = timedelta(0)
+        self._saved_work = timedelta(0)
+        self._saved_rest = timedelta(0)
 
     def start(self):
-        self._status = "Working"
         self._cycle_timestamps.append(datetime.now())
+        self._status = "Working"
 
     def rest(self):
-        self._status = "Resting"
-        self._carry_worked()
-        self._carry_rest()
+        self._save_cycle_work()
+        self._save_cycle_rest()
         self._cycle_timestamps.append(datetime.now())
+        self._status = "Resting"
 
     def continue_work(self):
-        # self._update_timers()
-        self._carry_rest()
+        self._save_cycle_rest()
         self.start()
-
-    def stop(self):
-        raise NotImplemented
+        
+    def _calculate_cycle_time(self):
+        time_passed = datetime.now() - self._cycle_timestamps[-1]
+        return time_passed
 
     def status(self):
         return self._status
 
-    # def times(self):
-    #     self._update_timers()
-    #     return self._total_worked, self._available_rest
-
     def work_time(self):
-        self._update_worked()
-        return self._carried_worked + self._current_worked
-
-    def _update_worked(self):
         if self._status == "Working":
-            self._current_worked = datetime.now() - self._cycle_timestamps[-1]
+            return self._saved_work + self._calculate_cycle_time()
         elif self._status == "Resting":
-            self._current_worked = timedelta(0)
+            return self._saved_work
 
-    def _carry_worked(self):
-        self._carried_worked += self._current_worked
+    def _save_cycle_work(self):
+        self._saved_work += self._calculate_cycle_time()
 
     def rest_time(self):
-        self._update_rest()
         if self._status == "Working":
-            available_rest = self._current_rest + self._carried_rest
+            return self._saved_rest + (self._calculate_cycle_time() / self._ratio)
         elif self._status == "Resting":
-            available_rest = self._carried_rest - self._current_rest
-        print(f"Available rest after update: {available_rest}")
-        if available_rest > timedelta(0):
-            return available_rest
-        else:
-            return timedelta(0)
+            return self._calculate_remaining_rest()
 
-    def _update_rest(self):
+    def _calculate_remaining_rest(self):
+        remaining_rest = self._saved_rest - self._calculate_cycle_time()
+        return remaining_rest if remaining_rest >= timedelta(0) else timedelta(0)
+
+    def _save_cycle_rest(self):
         if self._status == "Working":
-            self._current_rest = self._current_worked / self._ratio
+            self._saved_rest += (self._calculate_cycle_time() / self._ratio)
         elif self._status == "Resting":
-            self._current_rest = datetime.now() - self._cycle_timestamps[-1]
+            self._saved_rest = self._calculate_remaining_rest()
 
-    def _carry_rest(self):
-        if self._carried_rest - self._current_rest > timedelta(0):
-            self._carried_rest += self._current_rest
-        else:
-            self._carried_rest = timedelta(0)
-
-    # def _update_rest(self):
-    #     if self._status == "Working":
-    #         self._current_rest = self._current_worked / self._ratio
-    #         self._available_rest += self._current_rest
-    #     elif self._status == "Resting":
-    #         self._current_rest = datetime.now() - self._cycle_timestamps[-1]
-    #         self._available_rest -= self._current_rest
-    #         if self._available_rest < timedelta(0):
-    #             self._available_rest = timedelta(0)
-    #             self._overrest += self._current_rest
-                
-    # def _carry_rest(self):
-    #     self._carried_rest +=
-
-    # def _update_timers(self):
-    #     self._update_worked()
-    #     self._update_rest()
+    def work_and_rest_time(self):
+        return self.work_time(), self.rest_time()
