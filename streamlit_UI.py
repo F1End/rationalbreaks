2912abcd7db9831dd3094c091c85e838228b4037
F1End <horvirich@gmail.com>
2024-10-06 20:04:27 +0200

    -Created requirements.txt, streamlit_UI.py and sketched streamlit pseudo-code

---
+++ streamlit_UI.py
@@ -0,0 +1,42 @@
+import streamlit
+
+from rationalbreaks.timers import RatioNalTimer
+
+# create shared object for ui frontend, ui backend and timer
+
+# function for state update and timer update paralelization
+
+# create display frontend
+# header, etc.
+
+# function for timer update (loop)
+# vars: timer session
+# pols timer work and rest times
+# displays these in a reasonably large format
+# status display via ss?
+# loops indefinately (maybe interrupting brakes? if cannot work other way)
+
+# function for displaying initial not started state
+# button for settings and starting, each changing state and timer via one func
+
+# function for displaying settings state window conditions
+# filed for ratio entry, apply button, reset question pop with appropriate calls to timer
+# vars: timer session
+
+# functing for displaying work state
+# buttons: rest (biggest), settings, reset
+
+# function for displaying rest state
+# buttons: rest (biggest), settings, reset
+
+# function for reset
+# vars: timer session
+# replaces session with a new, empty one
+
+# main starts here
+
+# create session states
+# create list of states and store to ss
+# create timer instance
+
+# actual calling of functions dependent on session_state with display loop at the end
